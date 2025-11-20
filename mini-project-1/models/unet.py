1b2cd502ec0b38f5c75634f3c7ac5dbbf163f349
Dorianhgn <dorianhugonnet1@gmail.com>
2025-11-20 15:34:07 +0100

    Publication de la branche dev-segmentation

---
+++ mini-project-1/models/unet.py
@@ -0,0 +1,12 @@
+import torch
+import torch.nn as nn
+import torch.nn.functional as F
+
+class BasicConv2d(nn.Module):
+    pass
+
+class DoubleConv2d(nn.Module):
+    pass
+
+class UNet(nn.Module):
+    pass
